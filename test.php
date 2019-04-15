9f2e6e8f2ca19edcb42d5b84ae468a8d3f3b4d52
bushra zafar <bushrajaved9211@gmail.com>
2019-04-14 22:22:23 -0400

    added

---
+++ test.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- <?php echo phpinfo();
+
+            ?> -->
+
+    <?php
+    $a = 3;
+    $b = 6;
+    $c = 0;
+    $c = $a + $b;
+    echo $c
+    ?>
+</body>
+
+</html>
